4a72a2bd0f5d0e4f75e04c08128845a08f26b8e2
Christian Garbin <garbin.christian@gmail.com>
2021-01-26 05:53:20 -0500

    Let user pick a file

--- assignment1/question4_flip_negative.m
+++ assignment1/question4_flip_negative.m
@@ -1,3 +1,6 @@
-img = imread("cameraman.tif");
-
-montage({img, imcomplement(img), fliplr(img), flipud(img)}, 'Size', [2 2]);
+[file, path] = uigetfile({'*.png;*.jpg;*.jpeg;*.tif;*,bmp', 'Pictures'},...
+               'Select an image');
+if file ~= 0
+    img = imread([path file]);
+    montage({img, imcomplement(img), fliplr(img), flipud(img)}, 'Size', [2 2]);
+end
