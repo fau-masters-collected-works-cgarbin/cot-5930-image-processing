c2cab0ca33833ad1d23e2cb37b48db1cd10aaf6b
Christian Garbin <garbin.christian@gmail.com>
2021-01-25 20:24:38 -0500

    Improve comment

--- assignment1/question3_gray_levels.m
+++ assignment1/question3_gray_levels.m
@@ -3,4 +3,7 @@ img = imread("pout.tif");
 sprintf("Gray levels: maximum=%d, minimum=%d, average=%f", ...
     max(img(:)), min(img(:)), mean(img(:)))
 
-%imshow(img)
+% Note to self: "figure, " is not strictly necessary here because we have
+% only one picture to show
+% See https://www.mathworks.com/help/images/display-multiple-images.html
+figure, imshow(img)
