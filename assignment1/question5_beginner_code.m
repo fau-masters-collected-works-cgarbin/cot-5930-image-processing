472deaf0121f5f6b2f60aa183447e854f87d82eb
Christian Garbin <garbin.christian@gmail.com>
2021-01-26 04:41:02 -0500

    Improve explanation of changes

--- assignment1/question5_beginner_code.m
+++ assignment1/question5_beginner_code.m
@@ -15,8 +15,8 @@ fprintf("Elapsed time - beginner's code: %fs\n", tEnd)
 
 %% Better code
 % Vectorized operations
-% Solves the computational problem from the original code: using a loop
-% instead of vectorizing the code
+% Solves the computational problem from the original code: using a loop instead of
+% vectorizing the code
 Ib = imread('coins.png');
 tStart = tic;
 Jb = Ib * 0.5;
@@ -28,11 +28,11 @@ fprintf("Elapsed time - vectorized 1: %fs\n", tEnd)
 
 %% Betterer code
 % Vectorized operation, no intermediate variable, better UI
-% Improves the user interface by showing the original and modified images
-% side by side - as a bonus, it significantly improves the time to show
-% the images to the users (one window instead of two)
-% From the practical point of view, i.e. what the user perceives, the
-% change to `montage` is the largest improvement
+% Improves the UI by showing the original and modified images side by side
+% As a bonus, it is significantly improves the time to show the images to the users
+% (one to a grpahing/windowing function instead of two)
+% From the practical point of view, i.e. what the user perceives, the change to
+% `montage` is the largest improvement in responsiveness
 Ic = imread('coins.png');
 tStart = tic;
 montage([Ic, Ic * 0.5])
